a8c90806cd423f07a7c3428cf173a00c4a73a412
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-11-10 12:06:11 +0530

    Revamp Cors filter

--- src/main/java/lk/ijse/dep9/api/filter/CorsFilter.java
+++ src/main/java/lk/ijse/dep9/api/filter/CorsFilter.java
@@ -8,18 +8,27 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-@WebFilter(filterName = "cors-filter", urlPatterns = {"/members/*", "/books/*"})
+//@WebFilter(filterName = "cors-filter", urlPatterns = {"/members/*", "/books/*"})
 public class CorsFilter extends HttpFilter {
 
-    private final String[] origins = {"http://localhost", "http://34.100.220.186:8080"};
+    private String[] origins;
+
+    @Override
+    public void init() throws ServletException {
+        String origin = getFilterConfig().getInitParameter("origins");
+        origins = origin.split(", ");
+    }
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
 
         String requestedOrigin = req.getHeader("Origin");
         for (String origin : origins) {
-            if (requestedOrigin.startsWith(origin)){
+            if (requestedOrigin.startsWith(origin.trim())){
                 res.setHeader("Access-Control-Allow-Origin", requestedOrigin);
                 break;
             }
@@ -30,11 +39,11 @@ public class CorsFilter extends HttpFilter {
             res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, HEAD, POST, PATCH, DELETE");
 
             String requestedMethod = req.getHeader("Access-Control-Request-Method");
-            String requestedHeaders = req.getHeader("Access-Control-Request-Headers").toLowerCase();
+            String requestedHeaders = req.getHeader("Access-Control-Request-Headers");
 
             if ((requestedMethod.equalsIgnoreCase("POST") ||
                 requestedMethod.equalsIgnoreCase("PATCH")) &&
-                requestedHeaders.contains("content-type")){
+                requestedHeaders.toLowerCase().contains("content-type")){
                 res.setHeader("Access-Control-Allow-Headers", "Content-Type");
             }
         }else{
