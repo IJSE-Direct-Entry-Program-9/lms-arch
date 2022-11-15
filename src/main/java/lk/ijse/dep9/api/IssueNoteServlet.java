f596bd9ebd833fb69a1420aa5c92407497f928b9
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-11-15 14:14:21 +0530

    Finalize the create issue web service

--- src/main/java/lk/ijse/dep9/api/IssueNoteServlet.java
+++ src/main/java/lk/ijse/dep9/api/IssueNoteServlet.java
@@ -12,10 +12,9 @@ import lk.ijse.dep9.dto.IssueNoteDTO;
 
 import javax.sql.DataSource;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.stream.Collectors;
 
 @WebServlet(name = "IssueNoteServlet", value = "/issue-notes/*")
 public class IssueNoteServlet extends HttpServlet2 {
@@ -51,10 +50,16 @@ public class IssueNoteServlet extends HttpServlet2 {
             throw new JsonbException("Member id is empty or invalid");
         } else if (issueNote.getBooks().isEmpty()) {
             throw new JsonbException("Can't place an issue note without books");
+        }else if(issueNote.getBooks().size() > 3){
+            throw new JsonbException("Can't issue more than 3 books");
         } else if (issueNote.getBooks().stream()
-                .anyMatch(isbn -> !isbn.matches("([0-9][0-9\\\\-]*[0-9])"))) {
+                .anyMatch(isbn -> isbn == null || !isbn.matches("([0-9][0-9\\\\-]*[0-9])"))) {
             throw new JsonbException("Invalid isbn in the books list");
+        }else if(issueNote.getBooks().stream().collect(Collectors.toSet()).size()
+                != issueNote.getBooks().size()){
+            throw new JsonbException("Duplicate isbns are found");
         }
+
 
         try (Connection connection = pool.getConnection()) {
             PreparedStatement stmMemberExist = connection.
@@ -70,16 +75,27 @@ public class IssueNoteServlet extends HttpServlet2 {
                             "    INNER JOIN `return` r ON NOT (ii.issue_id = r.issue_id and ii.isbn = r.isbn)\n" +
                             "    RIGHT OUTER JOIN book b on ii.isbn = b.isbn WHERE b.isbn = ? GROUP BY b.isbn");
 
+            PreparedStatement stm2 = connection.prepareStatement("SELECT *, b.title FROM issue_item ii\n" +
+                    "    INNER JOIN `return` r ON NOT(ii.issue_id = r.issue_id and ii.isbn = r.isbn)\n" +
+                    "    INNER JOIN book b on ii.isbn = b.isbn\n" +
+                    "    INNER JOIN issue_note `in` on ii.issue_id = `in`.id\n" +
+                    "    WHERE `in`.member_id = ? AND b.isbn = ?");
+            stm2.setString(1, issueNote.getMemberId());
+
             for (String isbn : issueNote.getBooks()) {
                 stm.setString(1, isbn);
+                stm2.setString(2, isbn);
                 ResultSet rst = stm.executeQuery();
+                ResultSet rst2 = stm2.executeQuery();
                 if (!rst.next()) throw new JsonbException("Book doesn't exist");
                 if (!rst.getBoolean("availability")) {
                     throw new JsonbException(isbn + " is not available at the moment");
                 }
+                if (rst2.next()) throw new JsonbException("Book has been already issued to the same member before");
             }
 
-            PreparedStatement stmAvailable = connection.prepareStatement("SELECT m.name, 3 - COUNT(r.issue_id) as available FROM issue_note\n" +
+            PreparedStatement stmAvailable = connection.
+                    prepareStatement("SELECT m.name, 3 - COUNT(r.issue_id) as available FROM issue_note\n" +
                     "    INNER JOIN issue_item ii on issue_note.id = ii.issue_id\n" +
                     "    INNER JOIN `return` r ON NOT(ii.issue_id = r.issue_id and ii.isbn = r.isbn)\n" +
                     "    RIGHT OUTER JOIN member m on issue_note.member_id = m.id\n" +
@@ -92,6 +108,46 @@ public class IssueNoteServlet extends HttpServlet2 {
                 throw new JsonbException("Member can borrow only " + available + " books");
             }
 
+            try {
+                connection.setAutoCommit(false);
+                PreparedStatement stmIssueNote =
+                        connection.
+                                prepareStatement("INSERT INTO issue_note (date, member_id) VALUES (?,?)",
+                                        Statement.RETURN_GENERATED_KEYS);
+                stmIssueNote.setDate(1, Date.valueOf(LocalDate.now()));
+                stmIssueNote.setString(2, issueNote.getMemberId());
+                if (stmIssueNote.executeUpdate() != 1){
+                    throw new SQLException("Failed to insert the issue note");
+                }
+
+                ResultSet generatedKeys = stmIssueNote.getGeneratedKeys();
+                generatedKeys.next();
+                int issueNoteId = generatedKeys.getInt(1);
+
+                PreparedStatement stmIssueItem =
+                        connection.prepareStatement("INSERT INTO issue_item (issue_id, isbn) VALUES (?,?)");
+                stmIssueItem.setInt(1, issueNoteId);
+                for (String isbn : issueNote.getBooks()) {
+                    stmIssueItem.setString(2, isbn);
+                    if (stmIssueItem.executeUpdate() != 1){
+                        throw new SQLException("Failed to insert an issue item");
+                    }
+                }
+
+                connection.commit();
+                issueNote.setDate(LocalDate.now());
+                issueNote.setId(issueNoteId);
+                response.setStatus(HttpServletResponse.SC_CREATED);
+                response.setContentType("application/json");
+                JsonbBuilder.create().toJson(issueNote, response.getWriter());
+
+            }catch (Throwable t){
+                t.printStackTrace();
+                connection.rollback();
+                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to place the issue note");
+            }finally{
+                connection.setAutoCommit(true);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
             response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to place the issue note");
