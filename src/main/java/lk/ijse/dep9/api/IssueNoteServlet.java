a1a88db6a613c90b16d725ea2aab5bcc0e7944b5
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-11-15 18:45:45 +0530

    Finalize the IsseuNoteServlet

--- src/main/java/lk/ijse/dep9/api/IssueNoteServlet.java
+++ src/main/java/lk/ijse/dep9/api/IssueNoteServlet.java
@@ -35,115 +35,113 @@ public class IssueNoteServlet extends HttpServlet2 {
             }
 
             IssueNoteDTO issueNote = JsonbBuilder.create().fromJson(request.getReader(), IssueNoteDTO.class);
-            createIssueNote(issueNote, response);
-
+            createNewIssueNote(issueNote, response);
         } catch (JsonbException e) {
             response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
         }
-
     }
 
-    private void createIssueNote(IssueNoteDTO issueNote, HttpServletResponse response) throws IOException {
-        if (issueNote.getMemberId() == null ||
-                !issueNote.getMemberId()
-                        .matches("([A-Fa-f0-9]{8}(-[A-Fa-f0-9]{4}){3}-[A-Fa-f0-9]{12})")) {
-            throw new JsonbException("Member id is empty or invalid");
-        } else if (issueNote.getBooks().isEmpty()) {
-            throw new JsonbException("Can't place an issue note without books");
-        }else if(issueNote.getBooks().size() > 3){
-            throw new JsonbException("Can't issue more than 3 books");
-        } else if (issueNote.getBooks().stream()
-                .anyMatch(isbn -> isbn == null || !isbn.matches("([0-9][0-9\\\\-]*[0-9])"))) {
-            throw new JsonbException("Invalid isbn in the books list");
-        }else if(issueNote.getBooks().stream().collect(Collectors.toSet()).size()
-                != issueNote.getBooks().size()){
-            throw new JsonbException("Duplicate isbns are found");
+    private void createNewIssueNote(IssueNoteDTO issueNoteDTO, HttpServletResponse response) throws IOException {
+        /* Data Validation */
+        if (issueNoteDTO.getMemberId() == null ||
+                !issueNoteDTO.getMemberId().matches("([A-Fa-f0-9]{8}(-[A-Fa-f0-9]{4}){3}-[A-Fa-f0-9]{12})")) {
+            throw new JsonbException("The member id is empty or invalid");
+        } else if (issueNoteDTO.getBooks().isEmpty()) {
+            throw new JsonbException("A issue note requires at least one book");
+        } else if (issueNoteDTO.getBooks().size() > 3) {
+            throw new JsonbException("A member can't borrow more than 3 books");
+        } else if (!issueNoteDTO.getBooks().stream().
+                allMatch(isbn -> isbn.matches("([0-9][0-9\\\\-]*[0-9])"))) {
+            throw new JsonbException("Invalid isbn has been found");
+        } else if (issueNoteDTO.getBooks().stream().collect(Collectors.toSet()).size() !=
+                issueNoteDTO.getBooks().size()) {
+            throw new JsonbException("Duplicate isbn has been found");
         }
 
-
+        /* Business Validation */
         try (Connection connection = pool.getConnection()) {
-            PreparedStatement stmMemberExist = connection.
-                    prepareStatement("SELECT id FROM member WHERE id=?");
-            stmMemberExist.setString(1, issueNote.getMemberId());
-            if (!stmMemberExist.executeQuery().next()) {
-                throw new JsonbException("Member doesn't exist");
+            PreparedStatement stmExist = connection.prepareStatement("SELECT id FROM member WHERE id=?");
+            stmExist.setString(1, issueNoteDTO.getMemberId());
+            if (!stmExist.executeQuery().next()) {
+                throw new JsonbException("Member does not exist within the database");
             }
 
-            PreparedStatement stm = connection.
-                    prepareStatement("SELECT b.title, b.copies, ((b.copies - COUNT(ii.isbn)) > 0) as availability  " +
-                            "FROM issue_item ii\n" +
-                            "    INNER JOIN `return` r ON NOT (ii.issue_id = r.issue_id and ii.isbn = r.isbn)\n" +
-                            "    RIGHT OUTER JOIN book b on ii.isbn = b.isbn WHERE b.isbn = ? GROUP BY b.isbn");
+            PreparedStatement stm = connection.prepareStatement(
+    "SELECT b.title, ((b.copies - COUNT(issue_item.isbn)) > 0) as `availability` FROM issue_item " +
+            "    LEFT OUTER JOIN `return` r ON issue_item.issue_id = r.issue_id and issue_item.isbn = r.isbn " +
+            "    RIGHT OUTER JOIN book b on issue_item.isbn = b.isbn " +
+            "    WHERE r.date IS NULL and b.isbn = ? GROUP BY b.isbn");
 
-            PreparedStatement stm2 = connection.prepareStatement("SELECT *, b.title FROM issue_item ii\n" +
-                    "    INNER JOIN `return` r ON NOT(ii.issue_id = r.issue_id and ii.isbn = r.isbn)\n" +
-                    "    INNER JOIN book b on ii.isbn = b.isbn\n" +
-                    "    INNER JOIN issue_note `in` on ii.issue_id = `in`.id\n" +
-                    "    WHERE `in`.member_id = ? AND b.isbn = ?");
-            stm2.setString(1, issueNote.getMemberId());
+            PreparedStatement stmDuplicateExist = connection.prepareStatement(
+                    "SELECT name, ii.isbn " +
+                            "FROM member " +
+                            "         INNER JOIN issue_note `in` ON member.id = `in`.member_id " +
+                            "         INNER JOIN issue_item ii ON `in`.id = ii.issue_id " +
+                            "         LEFT OUTER JOIN `return` r ON ii.issue_id = r.issue_id and ii.isbn = r.isbn " +
+                            "WHERE r.date IS NULL AND member.id = ? AND ii.isbn = ?"
+            );
+            stmDuplicateExist.setString(1, issueNoteDTO.getMemberId());
 
-            for (String isbn : issueNote.getBooks()) {
+            for (String isbn : issueNoteDTO.getBooks()) {
                 stm.setString(1, isbn);
-                stm2.setString(2, isbn);
+                stmDuplicateExist.setString(2, isbn);
                 ResultSet rst = stm.executeQuery();
-                ResultSet rst2 = stm2.executeQuery();
-                if (!rst.next()) throw new JsonbException("Book doesn't exist");
+                ResultSet rst2 = stmDuplicateExist.executeQuery();
+                if (!rst.next()) throw new JsonbException(isbn + " book doesn't exist within the database");
                 if (!rst.getBoolean("availability")) {
-                    throw new JsonbException(isbn + " is not available at the moment");
+                    throw new JsonbException(isbn + " book is not available at the moment");
                 }
-                if (rst2.next()) throw new JsonbException("Book has been already issued to the same member before");
+                if (rst2.next()) throw new JsonbException(isbn + " book has been already issued to the same member");
             }
 
-            PreparedStatement stmAvailable = connection.
-                    prepareStatement("SELECT m.name, 3 - COUNT(r.issue_id) as available FROM issue_note\n" +
-                    "    INNER JOIN issue_item ii on issue_note.id = ii.issue_id\n" +
-                    "    INNER JOIN `return` r ON NOT(ii.issue_id = r.issue_id and ii.isbn = r.isbn)\n" +
-                    "    RIGHT OUTER JOIN member m on issue_note.member_id = m.id\n" +
-                    "    WHERE m.id = ? GROUP BY m.id");
-            stmAvailable.setString(1, issueNote.getMemberId());
-            ResultSet rst = stmAvailable.executeQuery();
+            PreparedStatement stm2 = connection.prepareStatement(
+                    "SELECT member.id, name, 3 - COUNT(`in`.id) as `available` FROM member " +
+                            "LEFT OUTER JOIN issue_note `in` ON member.id = `in`.member_id " +
+                            "LEFT OUTER JOIN issue_item ii ON `in`.id = ii.issue_id " +
+                            "LEFT OUTER JOIN `return` r ON ii.issue_id = r.issue_id and ii.isbn = r.isbn " +
+                            "WHERE r.date IS NULL AND member.id = ? GROUP BY member.id"
+            );
+            stm2.setString(1, issueNoteDTO.getMemberId());
+            ResultSet rst = stm2.executeQuery();
             rst.next();
             int available = rst.getInt("available");
-            if (issueNote.getBooks().size() > available) {
-                throw new JsonbException("Member can borrow only " + available + " books");
+            if (available < issueNoteDTO.getBooks().size()){
+                throw new JsonbException("Issue limit is exceeded, only " + available + " books are available");
             }
 
             try {
                 connection.setAutoCommit(false);
+
                 PreparedStatement stmIssueNote =
-                        connection.
-                                prepareStatement("INSERT INTO issue_note (date, member_id) VALUES (?,?)",
-                                        Statement.RETURN_GENERATED_KEYS);
+                        connection.prepareStatement("INSERT INTO issue_note (date, member_id) VALUES (?, ?)",
+                                Statement.RETURN_GENERATED_KEYS);
                 stmIssueNote.setDate(1, Date.valueOf(LocalDate.now()));
-                stmIssueNote.setString(2, issueNote.getMemberId());
-                if (stmIssueNote.executeUpdate() != 1){
-                    throw new SQLException("Failed to insert the issue note");
-                }
+                stmIssueNote.setString(2, issueNoteDTO.getMemberId());
+                if (stmIssueNote.executeUpdate() != 1) throw new SQLException("Failed to insert the issue note");
 
                 ResultSet generatedKeys = stmIssueNote.getGeneratedKeys();
                 generatedKeys.next();
                 int issueNoteId = generatedKeys.getInt(1);
 
                 PreparedStatement stmIssueItem =
-                        connection.prepareStatement("INSERT INTO issue_item (issue_id, isbn) VALUES (?,?)");
+                        connection.prepareStatement("INSERT INTO issue_item (issue_id, isbn) VALUES (?, ?)");
                 stmIssueItem.setInt(1, issueNoteId);
-                for (String isbn : issueNote.getBooks()) {
+                for (String isbn : issueNoteDTO.getBooks()) {
                     stmIssueItem.setString(2, isbn);
-                    if (stmIssueItem.executeUpdate() != 1){
-                        throw new SQLException("Failed to insert an issue item");
-                    }
+                    if (stmIssueItem.executeUpdate() != 1) throw new SQLException("Failed to insert the issue item");
                 }
 
                 connection.commit();
-                issueNote.setDate(LocalDate.now());
-                issueNote.setId(issueNoteId);
+
+                issueNoteDTO.setDate(LocalDate.now());
+                issueNoteDTO.setId(issueNoteId);
                 response.setStatus(HttpServletResponse.SC_CREATED);
                 response.setContentType("application/json");
-                JsonbBuilder.create().toJson(issueNote, response.getWriter());
+                JsonbBuilder.create().toJson(issueNoteDTO, response.getWriter());
 
             }catch (Throwable t){
-                t.printStackTrace();
                 connection.rollback();
+                t.printStackTrace();
                 response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to place the issue note");
             }finally{
                 connection.setAutoCommit(true);
