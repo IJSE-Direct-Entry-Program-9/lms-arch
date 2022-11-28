180d1f71ec91eeffa725b7fc524d5ccb1a7c1556
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-11-28 18:26:05 +0530

    Finalize the logging

--- src/main/java/lk/ijse/dep9/api/util/HttpServlet2.java
+++ src/main/java/lk/ijse/dep9/api/util/HttpServlet2.java
@@ -5,12 +5,16 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.dep9.api.exception.ValidationException;
 import lk.ijse.dep9.dto.ResponseStatusDTO;
 import lk.ijse.dep9.exception.ResponseStatusException;
+import lk.ijse.dep9.service.exception.*;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.util.Date;
 
+@Slf4j
 public class HttpServlet2 extends HttpServlet {
 
     protected void doPatch(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -26,18 +30,24 @@ public class HttpServlet2 extends HttpServlet {
                 super.service(req, resp);
             }
         } catch (Throwable t) {
-            ResponseStatusException r = t instanceof ResponseStatusException ?
-                    (ResponseStatusException) t : null;
+            ResponseStatusException r = t instanceof ResponseStatusException ? (ResponseStatusException) t : null;
 
             if (r == null || r.getStatus() >= 500) {
-                t.printStackTrace();
+                log.error(t.getMessage(), t);
             }
 
-            ResponseStatusDTO statusDTO = new ResponseStatusDTO(
-                    r == null ? 500 : r.getStatus(),
-                    t.getMessage(),
-                    req.getRequestURI(),
-                    new Date().getTime());
+            ResponseStatusDTO statusDTO = new ResponseStatusDTO(r == null ? 500 : r.getStatus(), t.getMessage(), req.getRequestURI(), new Date().getTime());
+
+            if (t instanceof ValidationException ||
+                    t instanceof LimitExceedException ||
+                    t instanceof AlreadyReturnedException ||
+                    t instanceof AlreadyIssuedException) {
+                statusDTO.setStatus(400);
+            } else if (t instanceof NotFoundException) {
+                statusDTO.setStatus(404);
+            } else if (t instanceof InUseException || t instanceof DuplicateException) {
+                statusDTO.setStatus(409);
+            }
 
             resp.setContentType("application/json");
             resp.setStatus(statusDTO.getStatus());
